509ed8369b2a5070bd0ce5bcae34d95ad2cb9697
Soundoverlord <soundoverlord93@gmail.com>
2022-02-23 11:13:21 +0200

    load like a grep program

---
+++ like_a_grep.py
@@ -0,0 +1,23 @@
+import argparse
+from termcolor import colored
+
+ap = argparse.ArgumentParser(description=__doc__)
+ap.add_argument("-p", "--pattern", required=True,  type=str, help="please enter searching value")
+ap.add_argument("-f", "--file", required=True,  type=str, help=r"C:\txt1.txt")
+args = vars(ap.parse_args())
+
+
+try:
+    t_file = open(args['file'])
+except FileNotFoundError:
+    print(colored("File not found, please enter existing file name with file path", "red"))
+else:
+    f_data = t_file.readlines()
+    #print(type(f_data[0]))
+    for f_line in f_data:
+        if args['pattern'] in f_line:
+            #print(type(f_data))
+            f_line = f_line.replace(args['pattern'],colored(args['pattern'],"yellow"))
+            print (f"{f_line}")
+            #print(args['pattern'])
+
